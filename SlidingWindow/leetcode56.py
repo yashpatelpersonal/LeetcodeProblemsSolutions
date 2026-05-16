24ec18a937792884f1c9b8f4802a80b09e50088e
Yashkumar Patel <yash@aarogram.com>
2026-05-16 13:00:09 -0500

    Daily Problems 154, Leetcode 56

--- SlidingWindow/leetcode56.py
+++ SlidingWindow/leetcode56.py
@@ -1,25 +1,15 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        intervals=sorted(intervals)
-        if len(intervals)<=1:
-            return intervals
-        result=[]
-        start,end=intervals[0][0],intervals[0][1]
-        for k in range(1,len(intervals)):
-            if end >= intervals[k][0] and end <= intervals[k][1] :
-                end=max(intervals[k][1],end)
-                start=min(start,intervals[k][0])
-                result.append([start,end])
-            elif len(intervals) == 2:
-                result.append([start,end])
-                result.append([intervals[k][0],intervals[k][1]])
-            else :
-                if intervals[k][0] <= start and intervals[k][1]>=end:
-                    start=intervals[k][0]
-                    end=intervals[k][1]
-                result.append([start,end])
-        return result
-        # return res
+        intervals.sort(key =lambda i : i[0] )       # return res
+        return_=[intervals[0]]
+        for start,end in intervals[1:]:
+            last_val=return_[-1][1]
+            if start <= last_val:
+                return_[-1][1]=max(last_val,end)
+            else:
+                return_.append([start,end])
+        return return_
+
 
 
 # if __name__ == '__main__':
